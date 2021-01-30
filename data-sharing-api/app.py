74c25fdf89234de425a1fe8333ec0789a30705d9
sapinspys <garri009@fiu.edu>
2021-01-30 12:03:08 -0500

    Begun migrating logic to Chalice app

--- data-sharing-api/app.py
+++ data-sharing-api/app.py
@@ -1,29 +1,22 @@
+import os
+import json
+import math
+import pandas as pd
+import psycopg2-binary
+import cryptography
 from chalice import Chalice
 
 app = Chalice(app_name='data-sharing-api')
 
+HOST = os.environ['HOST']
+USER = os.environ['USER']
+PASSWORD = os.environ['PASSWORD']
+AUTH_PWD = os.environ['AUTH_PWD']
+ENCRYPTION_KEY = str.encode(os.environ["ENCRYPTION_KEY"])
+
 
 @app.route('/')
 def index():
-    return {'hello': 'world'}
+    return {'Status': 'OK'}
 
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+# @app.route('/guests')
